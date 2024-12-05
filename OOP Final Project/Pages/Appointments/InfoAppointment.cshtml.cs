fcb4573fabfd20c43080451a37ffc9ad7b5bddb3
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-05 16:35:30 +0700

    Dashboard Patient

---
+++ OOP Final Project/Pages/Appointments/InfoAppointment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Appointments
+{
+    public class InfoAppointmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
