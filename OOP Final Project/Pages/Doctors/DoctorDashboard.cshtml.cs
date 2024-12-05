f8041113af6ce54864afcaad9fc136824edbf8f2
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-05 23:25:27 +0700

    Doctor Dashboard

---
+++ OOP Final Project/Pages/Doctors/DoctorDashboard.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP_Final_Project.Pages.Doctors
+{
+    public class DoctorDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
