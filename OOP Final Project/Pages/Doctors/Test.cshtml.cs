3c2a2c908add6cbba3506e067043a6eca8ba07d3
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-08 19:22:00 +0700

    update layout

---
+++ OOP Final Project/Pages/Doctors/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Doctors
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
