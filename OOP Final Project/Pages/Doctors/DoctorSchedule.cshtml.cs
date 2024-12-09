fd8b1e5e511ec27cfb52dce024f6978c1ad8feab
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-09 22:09:32 +0700

    Doctor Schedule

---
+++ OOP Final Project/Pages/Doctors/DoctorSchedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Doctors
+{
+    public class DoctorScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
