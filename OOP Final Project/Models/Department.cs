5e7581ab5b25b3443ba0ae3cc57683a289bc1be6
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-10 23:38:19 +0700

    Schedule & Department

---
+++ OOP Final Project/Models/Department.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OOP_Final_Project.Models;
+
+public class Department
+{
+    /* 
+    ! This model is used to define the departments within a clinic.
+    - Id: The unique identifier of the department.
+    - ClinicId: The foreign key linking to the clinic where the department belongs.
+    - Name: The name of the department.There are 4 departments in the clinic: Reception, Dental, Medical, and Management.
+
+    ? A department can belong to a specific clinic and may contain many employees.
+    */
+
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey("Clinic")]
+    public int ClinicId { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+}
---
+++ OOP Final Project/Models/Schedule.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OOP_Final_Project.Models;
+
+public class Schedule
+{
+    /* 
+    ! This model is used to define the schedule for employees.
+    - Id: The unique identifier of the schedule.
+    - Date: The day of the week (e.g., Monday, Tuesday).
+    - TimeStart: The starting time of the schedule.
+    - TimeEnd: The ending time of the schedule.
+
+    ? Schedules help assign time slots to employees for their shifts or duties.
+    */
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public DayOfWeek Date { get; set; }
+
+    [Required]
+    public required TimeSpan TimeStart { get; set; }
+
+    [Required]
+    public required TimeSpan TimeEnd { get; set; }
+}
