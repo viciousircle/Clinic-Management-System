0d3e20b4ffc839edc3714ea0505a3a9179f75820
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-10 23:46:09 +0700

    Create Patient.cs

---
+++ OOP Final Project/Models/Patient.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OOP_Final_Project.Models;
+
+public class Patient
+{
+    /* 
+    ! This model is used to define the patients in the system.
+    - Id: The unique identifier of the patient.
+    - FirstName: The first name of the patient.
+    - LastName: The last name of the patient.
+    - Email: The email address of the patient.
+    - Phone: The phone number of the patient.
+    - Address: The address of the patient.
+    - UserName: The username of the patient for login. (6 months for registration)
+    - Password: The password for authentication.
+
+    ? A patient can have appointments and prescriptions.
+    */
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public required string FirstName { get; set; }
+
+    [Required]
+    public required string LastName { get; set; }
+
+    [Required]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string Phone { get; set; }
+
+    [Required]
+    public required string Address { get; set; }
+
+    [Required]
+    public required string UserName { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+}
