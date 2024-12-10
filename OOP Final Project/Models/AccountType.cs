23ddecd562af63258d574b0c3138ee8a1342b04c
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-10 23:21:04 +0700

    Create AccountType.cs

---
+++ OOP Final Project/Models/AccountType.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OOP_Final_Project.Models;
+
+public class AccountType
+{
+    /* 
+    ! This model is used to define the types of accounts in the system.
+    - Id: The unique identifier of the account type.
+    - Name: There are 2 types of accounts in the system: Limited and Indefinitely.
+
+    ? Different account types determine the permissions of the users.
+    - Employees account is indefinitely account type.
+    - Patients account is a limited account type. (6 months)
+    */
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+}
