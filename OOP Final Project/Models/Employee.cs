e3a68b713ae94f6a39545e9908fad17e7fc51ef1
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-10 23:42:45 +0700

    Create Employee.cs

---
+++ OOP Final Project/Models/Employee.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OOP_Final_Project.Models;
+
+public class Employee
+{
+    /* 
+    ! This model is used to define the employees in the system.
+    - Id: The unique identifier of the employee.
+    - FirstName: The first name of the employee.
+    - LastName: The last name of the employee.
+    - UserName: The username of the employee for login.
+    - Password: The password for authentication.
+    - DepartmentId: The foreign key linking to the department the employee belongs to.
+    - RoleId: The foreign key linking to the role of the employee.
+    - Email: The email address of the employee.
+    - Phone: The phone number of the employee.
+    - IsActive: Whether the employee is working for the company or not.
+
+    ? An employee has a role and belongs to a department.
+    */
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public required string FirstName { get; set; }
+
+    [Required]
+    public required string LastName { get; set; }
+
+    [Required]
+    public required string UserName { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+
+    [ForeignKey("Department")]
+    public int DepartmentId { get; set; }
+
+    [ForeignKey("Role")]
+    public int RoleId { get; set; }
+
+    [Required]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string Phone { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
