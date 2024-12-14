29d3df22ae60beef357e695ebeb6e985309eb5c8
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-14 22:24:51 +0700

    Test Databinding

---
+++ OOP Final Project/Pages/Test.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OOP_Final_Project.Data;
+using OOP_Final_Project.Models;
+
+namespace OOP_Final_Project.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
