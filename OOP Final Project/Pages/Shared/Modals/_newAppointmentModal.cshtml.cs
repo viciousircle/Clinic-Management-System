bfb836a625fde6be2bfbf41518d0b9ec5cd154ac
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-20 23:55:12 +0700

    Update Modal

---
+++ OOP Final Project/Pages/Shared/Modals/_newAppointmentModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Shared.Modals
+{
+    public class _newAppointmentModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
