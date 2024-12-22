7ec8761b4b05b40b9637247bf81e004ebe510062
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-22 11:59:45 +0700

    _prescribeModal

---
+++ OOP Final Project/Pages/Shared/Modals/_prescribeDetailsModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Shared.Modals
+{
+    public class _prescribeDetailsModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
