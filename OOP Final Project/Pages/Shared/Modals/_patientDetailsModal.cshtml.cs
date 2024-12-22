7408c85b890cd3478e69036b7d499c8ad714577a
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-22 10:28:14 +0700

    patientDetail

---
+++ OOP Final Project/Pages/Shared/Modals/_patientDetailsModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Shared.Modals
+{
+    public class _patientDetailsModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
