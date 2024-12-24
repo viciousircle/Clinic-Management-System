96dc55019f012ecc82d282b5a696b234f8891092
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-24 23:36:58 +0700

    Appointment Detail

---
+++ OOP Final Project/Pages/Shared/Modals/_appointmentDetailsModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Shared.Modals
+{
+    public class _appointmentDetailsModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
