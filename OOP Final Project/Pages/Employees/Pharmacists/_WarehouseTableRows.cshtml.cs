c322b5915cf134cfe5a21b91256454943bc70925
viciousircle <quy.vtm227257@sis.hust.edu.vn>
2024-12-28 07:38:06 +0700

    .

---
+++ OOP Final Project/Pages/Employees/Pharmacists/_WarehouseTableRows.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_Final_Project.Pages.Employees.Pharmacists
+{
+    public class _WarehouseTableRowsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
